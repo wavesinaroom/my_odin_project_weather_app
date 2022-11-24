8f4755278246f6983e39e1287dbe3bb40c8c14a6
wavesinaroom <juan@wavesinaroom.com>
2022-11-24 15:12:50 -0500

    [Feature] Fetch latitude and longitude form the Geocoding API

--- weather.js
+++ weather.js
@@ -1,10 +1,15 @@
-const request = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=e942fee8ce99d54e6ce8e15ee38866d4';
+let weatherRequest = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=e942fee8ce99d54e6ce8e15ee38866d4';
+let geoRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=Berlin&appid=e942fee8ce99d54e6ce8e15ee38866d4';
+let city;
 
-fetch(request)
+fetch(geoRequest)
   .then(function(response){
     alert('Did it');
+    return response.json();
   })
-
+  .then(function(response){
+    console.log(response[0].lat);
+  })
   .catch(function(err){
     alert('Failed');
   });
