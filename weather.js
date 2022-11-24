87efbe3446171e6538b50a3c31504412d71467bb
wavesinaroom <juan@wavesinaroom.com>
2022-11-24 17:14:08 -0500

    [Task] Wrap code in a function - test use cases

--- weather.js
+++ weather.js
@@ -1,25 +1,24 @@
-let geoRequest = 'http://api.openweathermap.org/geo/1.0/direct?q=Berlin&appid=e942fee8ce99d54e6ce8e15ee38866d4';
-let city;
-
-fetch(geoRequest)
-  .then(function(response){
-    alert('Did it');
-    return response.json();
-  })
-  .then(function(response){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${response[0].lat}&lon=${response[0].lon}&appid=e942fee8ce99d54e6ce8e15ee38866d4`,{mode: 'cors'})
-      .then(function(response){
-        return response.json();
-      })
-      .then(function(response){
-        console.log(response);
-      })
-      .catch(function(err){
-        alert('No weather');
-      });
-  })
+function fetchWeatherData(city){
+  const geoRequest = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=e942fee8ce99d54e6ce8e15ee38866d4`;
+  fetch(geoRequest)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(response){
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${response[0].lat}&lon=${response[0].lon}&appid=e942fee8ce99d54e6ce8e15ee38866d4`,{mode: 'cors'})
+        .then(function(response){
+          return response.json();
+        })
+        .then(function(response){
+          console.log(response);
+        })
+        .catch(function(err){
+          alert('Sorry, an error just ocurred');
+        });
+    })
   .catch(function(err){
-    alert('Failed');
+    alert('Unknown city');
   });
-console.dir(city);
+}
 
+fetchWeatherData('lkdjfg');
