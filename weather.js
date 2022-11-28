4eefd483e1dfdd0df3f3a6bfd77789f1c09da50d
wavesinaroom <juan@wavesinaroom.com>
2022-11-28 12:36:53 -0500

    [Task] Remove attempt to implement loading bar -> might be resumed at the end of the project

--- weather.js
+++ weather.js
@@ -6,7 +6,6 @@ function fetchData(city) {
     })
     .then(function(response){
       fetchWeather(response[0].lat, response[0].lon);
-      setInterval(displayClock,1000);
     })
     .catch(function (err) {
       alert("Unknown city");
@@ -16,12 +15,13 @@ function fetchData(city) {
 function fetchWeather(lat, lon){
   const weatherRequest = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=e942fee8ce99d54e6ce8e15ee38866d4`;
   fetch(weatherRequest,{ mode: "cors" })
-    .then(function (response) {
-      return response.json();
+    .then(async function (response) {
+       return response.json();
     })
     .then(function (response) {
       displayWeather(response);
-      console.dir(response);
+      displayClock();
+      setInterval(displayClock,1000);
     })
     .catch(function (err) {
       alert("Sorry, an error just ocurred");
@@ -44,7 +44,7 @@ function displayWeather(weather){
 
 function displayClock(){
   const now = new Date();
-  document.getElementById('clock').innerHTML = `${now.getHours()%12||12}:${now.getMinutes()<10?'0'+now.getMinutes():now.getMinutes()}:${now.getSeconds()<10?'0'+now.getSeconds():now.getSeconds()}`;
+  document.getElementById('clock').innerHTML = `Local time ${now.getHours()%12||12}:${now.getMinutes()<10?'0'+now.getMinutes():now.getMinutes()}:${now.getSeconds()<10?'0'+now.getSeconds():now.getSeconds()}`;
 }
 
 fetchData('Berlin');
