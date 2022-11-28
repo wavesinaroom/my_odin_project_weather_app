5c66b5be9edc3221ec6ee19706436f55c92698c8
wavesinaroom <juan@wavesinaroom.com>
2022-11-28 18:08:58 -0500

    [Feature] Show info based on temperature scale user's input on first request -> next step: set temperature scale for further requests

--- weather.js
+++ weather.js
@@ -1,6 +1,6 @@
 let start, end;
 
-function fetchData(city) {
+function fetchData(city, units) {
   const geoRequest = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=e942fee8ce99d54e6ce8e15ee38866d4`;
   fetch(geoRequest)
     .then(function (response) {
@@ -8,15 +8,15 @@ function fetchData(city) {
       return response.json();
     })
     .then(function(response){
-      fetchWeather(response[0].lat, response[0].lon);
+      fetchWeather(response[0].lat, response[0].lon, units);
     })
     .catch(function (err) {
       alert("Unknown city");
     });
 }
 
-function fetchWeather(lat, lon){
-  const weatherRequest = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=e942fee8ce99d54e6ce8e15ee38866d4`;
+function fetchWeather(lat, lon, units){
+  const weatherRequest = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=e942fee8ce99d54e6ce8e15ee38866d4`;
   fetch(weatherRequest,{ mode: "cors" })
     .then(async function (response) {
        return response.json();
@@ -25,7 +25,7 @@ function fetchWeather(lat, lon){
       console.dir(response);
       end = window.performance.now();
       displayWeather(response);
-      displayClock();
+      displayClock(response);
       setInterval(displayClock,1000);
     })
     .catch(function (err) {
@@ -42,31 +42,43 @@ function displayWeather(weather){
                               <p>longitude: ${weather.coord.lon}</p>
                               <p>${weather.weather[0].description}</p>
                               <img alt='weather-icon' src=https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png></img>
-                              <p>${weather.main.temp}</p>
-                              <p>${weather.wind.speed}</p>
+                              <p>temperature: ${weather.main.temp}</p>
+                              <p>min temperature: ${weather.main.temp_min}</p>
                               <p>Fetched in ${end-start} ms</p> 
                               </div>`;
 }
 
-function displayClock(){
-  const now = new Date();
+function displayClock(weather){
+  let now = new Date();
   document.getElementById('clock').innerHTML = `Local time ${now.getHours()%12||12}:${now.getMinutes()<10?'0'+now.getMinutes():now.getMinutes()}:${now.getSeconds()<10?'0'+now.getSeconds():now.getSeconds()}`;
+}
+
+function tempFormatOnLoad(){
+  if(document.getElementById('celsius').checked === true){
+    return 'metric';
+  }else if(document.getElementById('farenheit').checked === true){
+    return 'imperial';
+  }else{
+    return undefined; 
+  }
 }
 
 const renderUserForm = function(){
   document.body.innerHTML = `<form>
                             <input type='text' placeholder='Type your city' id='user-input'></input>
-                            <label for='units'>Choose your units</label>
                             <div id='units-selector'>
-                            <input type = 'radio'>Metrics</input>
-                            <input type = 'radio'>Imperial</input>
+                            <input id='farenheit' type = 'radio' name = 'temperature'>F°</input>
+                            <input id='celsius' type = 'radio' name = 'temperature'>C°</input>
                             </div>
                             </form>`
   document.body.firstChild.addEventListener('submit', (e)=>{
-    fetchData(document.getElementById('user-input').value);
     e.preventDefault();
+    if(tempFormatOnLoad()!=undefined)
+      fetchData(document.getElementById('user-input').value,tempFormatOnLoad());
+    else
+      alert('no temp scale');
+      return;
   });
 
 }();
 
-//fetchData('Berlin');
